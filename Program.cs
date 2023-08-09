4d46ca2e8bd0635811c57172fdf56b889f3031fb
Ashish Rathore <ashrathore98@gmail.com>
2023-08-10 00:56:09 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        RainbowSchoolSystem schoolSystem = new RainbowSchoolSystem();
+
+        // Add sample data
+        schoolSystem.AddStudent(new Student { Name = "Ashish", ClassSection = "A" });
+        schoolSystem.AddStudent(new Student { Name = "Beena", ClassSection = "B" });
+
+        schoolSystem.AddTeacher(new Teacher { Name = "Ronaldo", ClassSection = "A" });
+        schoolSystem.AddTeacher(new Teacher { Name = "Messi", ClassSection = "B" });
+
+        schoolSystem.AddSubject(new Subject { Name = "Math", SubjectCode = "MATH101", Teacher = (Teacher)schoolSystem.Teachers });
+        schoolSystem.AddSubject(new Subject { Name = "Science", SubjectCode = "SCI101", Teacher = (Teacher)schoolSystem.Teachers });
+
+        // Display students in a class
+        schoolSystem.DisplayStudentsInClass("A");
+
+        // Display subjects taught by a teacher
+        schoolSystem.DisplaySubjectsTaughtByTeacher("Ms. Nikku");
+    }
+}
---
+++ RainbowSchoolSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RainbowSchoolSystem
+{
+    private List<Student> students = new List<Student>();
+    private List<Teacher> teachers = new List<Teacher>();
+    private List<Subject> subjects = new List<Subject>();
+
+    public object Teachers { get; internal set; }
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+
+    public void AddTeacher(Teacher teacher)
+    {
+        teachers.Add(teacher);
+    }
+
+    public void AddSubject(Subject subject)
+    {
+        subjects.Add(subject);
+    }
+
+    public void DisplayStudentsInClass(string classSection)
+    {
+        Console.WriteLine($"Students in class {classSection}:");
+        foreach (var student in students)
+        {
+            if (student.ClassSection == classSection)
+            {
+                Console.WriteLine($"- {student.Name}");
+            }
+        }
+    }
+
+    public void DisplaySubjectsTaughtByTeacher(string teacherName)
+    {
+        Console.WriteLine($"Subjects taught by {teacherName}:");
+        foreach (var subject in subjects)
+        {
+            if (subject.Teacher.Name == teacherName)
+            {
+                Console.WriteLine($"- {subject.Name}");
+            }
+        }
+    }
+}
